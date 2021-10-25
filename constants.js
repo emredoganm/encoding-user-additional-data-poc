3cea9fb944b8cd98facf1384b37f2d688a397af3
Emre Dogan <emredoganm@live.com>
2021-10-25 16:33:25 +0300

    Create constants for generic values

---
+++ constants.js
@@ -0,0 +1,11 @@
+const AuthorizationCookieName = "__Awesome_Authorization_Cookie";
+const AuthenticationCookieName = "__Authentication_COOKIE";
+const AuthenticationTokenExpireTime = "1m";
+const AuthorizationTokenExpireTime = "1d";
+
+module.exports = {
+  AuthenticationCookieName,
+  AuthorizationCookieName,
+  AuthorizationTokenExpireTime,
+  AuthenticationTokenExpireTime,
+};
