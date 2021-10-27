1e54827f8d2c55630ceb12117ee8f5268c87d958
Emre Dogan <emredoganm@live.com>
2021-10-27 12:48:14 +0300

    Add new routes for v1 version

--- routes/v1.js
+++ routes/v1.js
@@ -5,4 +5,28 @@ router.get("/", (_request, response) => {
   response.json({ message: "Hello World!" });
 });
 
+router.get("/subdomains/:siteName", (request, response) => {
+  response.json({ siteName: request.params.siteName, ...require("../responses/subdomains.json") });
+});
+
+router.get("/countries", (_request, response) => {
+  response.json(require("../responses/countries.json"));
+});
+
+router.get("/context/secondary-market/windows", (_request, response) => {
+  response.json(require("../responses/secondaryMarketWindows.json"));
+});
+
+router.get("/users/profile-status", (_request, response) => {
+  response.json(require("../responses/profileStatus.json"));
+});
+
+router.get("/investments/:id", (request, response) => {
+  response.json({ id: request.params.id, ...require("../responses/investment-details.json") });
+});
+
+router.get("/investments", (_request, response) => {
+  response.json(require("../responses/investments.json"));
+});
+
 module.exports = router;
