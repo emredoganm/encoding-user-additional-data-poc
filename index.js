2a768312f798023b6c3ade1f71530467bd65e6f7
Emre Dogan <emredoganm@live.com>
2021-10-27 10:05:59 +0300

    :construction: Change express app route configuration

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ require("dotenv").config();
 
 const express = require("express");
 const morgan = require("morgan");
-const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 
-const { generateUser, generateCustomData } = require("./utils");
-const { needsToBeAuthenticated, needsToBeVerified, needsToBeVIP } = require("./middlewares");
-const {
-  AuthorizationCookieName,
-  AuthenticationCookieName,
-  AuthenticationTokenExpireTime,
-  AuthorizationTokenExpireTime,
-} = require("./constants");
+const { v1Router, generalRouter } = require("./routes");
 
-const { PORT, API_PORT, CUSTOM_TOKEN_KEY, AUTHORIZATION_TOKEN_KEY } = process.env;
+const { PORT, API_PORT } = process.env;
 const port = PORT || API_PORT;
 const app = express();
 
@@ -24,41 +16,8 @@ app.use(cookieParser());
 app.use(morgan("combined"));
 app.use(helmet());
 
-app.get("/", (_request, response) => {
-  response.json({ message: "Hello World!" });
-});
-
-app.post("/login", (_request, response) => {
-  const data = generateUser();
-  const token = jwt.sign(data, AUTHORIZATION_TOKEN_KEY, {
-    expiresIn: AuthenticationTokenExpireTime,
-  });
-
-  response
-    .cookie(AuthenticationCookieName, token, { httpOnly: true, domain: "localhost" })
-    .json({ token, data });
-});
-
-app.all("/whoami", [needsToBeAuthenticated], (request, response) => {
-  response.json({ user: request.user });
-});
-
-app.post("/authorize", (request, response) => {
-  const payload = generateCustomData({ ...request.body });
-  const token = jwt.sign(payload, CUSTOM_TOKEN_KEY, { expiresIn: AuthorizationTokenExpireTime });
-
-  response
-    .cookie(AuthorizationCookieName, token, { httpOnly: true, domain: "localhost" })
-    .json({ token });
-});
-
-app.all("/authorized", [needsToBeAuthenticated, needsToBeVerified], (request, response) => {
-  response.json({ user: request.user });
-});
-
-app.all("/vip", [needsToBeAuthenticated, needsToBeVIP], (request, response) => {
-  response.json({ user: request.user });
-});
+app.use("/api/v1", v1Router);
+app.use("/", generalRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
